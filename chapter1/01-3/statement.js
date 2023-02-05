a5c5a1bb15c1af6f70dfea0ffbca76a760f079a2
HyoJin <121423610+oneday0230@users.noreply.github.com>
2023-02-06 00:29:47 +0900

    chapter1 - #1.4 statement() refactoring
    ~p.42

--- chapter1/01-3/statement.js
+++ chapter1/01-3/statement.js
@@ -27,24 +27,14 @@ export function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    //const play = playFor(perf); // 우변을 함수로 추출 -> 인라인된 변수는 제거 (6.4절 변수 인라인하기 참조)
-    let thisAmount = amountFor(perf, playFor(perf)); // 추출한 함수를 이용
-
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === playFor(perf).type) {
-      // 변수 인라인(playFor)
-      volumeCredits += Math.floor(perf.audience / 5);
-    }
-
+    //const play = playFor(perf); -> 인라인된 변수는 제거 (6.4절 변수 인라인하기 참조)
+    //let thisAmount = amountFor(perf, playFor(perf)); -> 인라인처리하여 변수 제거
+    
+    volumeCredits += volumeCreditsFor(perf);   // 추출한 함수를 이용해 값을 누적
+    
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(thisAmount / 100)} ${
-      // 변수 인라인(playFor)
-      perf.audience
-    }석\n`;
-    totalAmount += thisAmount;
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} ${perf.audience}석\n`;   // 변수 인라인(playFor, amountFor)
+    totalAmount += amountFor(perf);   // 변수 인라인(amountFor)
   }
   result += `총액 ${format(totalAmount / 100)}\n`;
   result += `적립 포인트 ${volumeCredits}점\n`;
@@ -56,13 +46,23 @@ function playFor(aPerformance) {
   return plays[aPerformance.playID];
 }
 
-function amountFor(aPerformance) {
-  // 값이 바뀌지 않는 변수 (aPerformance, play)는 매개변수로 전달 -> 변수 인라인 리팩토링하여 play 매개변수 삭제
+function volumeCreditsFor(aPerformance) {   // 새로 추출한 함수
+  let volumeCredits = 0;
+  
+  volumeCredits += Math.max(aPerformance.audience - 30, 0);
+
+  if ('comedy' === playFor(aPerformance).type) {  // 변수 인라인(playFor)
+    volumeCredits += Math.floor(aPerformance.audience / 5);
+  }
+
+  return volumeCredits;
+}
+
+function amountFor(aPerformance) {  // 변수 인라인 리팩토링으로 play 매개변수 삭제
   let result = 0; // 변수를 초기화 하는 코드 thisAmount -> result 명확한 이름으로 변경
 
-  switch (
-    playFor(aPerformance).type // play를 playFor() 호출로 변경
-  ) {
+  switch (playFor(aPerformance).type) // play를 playFor() 호출로 변경
+  {
     case 'tragedy': // 비극
       result = 40_000;
 
@@ -70,7 +70,7 @@ function amountFor(aPerformance) {
         result += 1_000 * (aPerformance.audience - 30);
       }
       break;
-    case 'comedy': // 희극
+    case 'comedy':  // 희극
       result = 30_000;
 
       if (aPerformance.audience > 20) {
@@ -79,7 +79,7 @@ function amountFor(aPerformance) {
       result += 300 * aPerformance.audience;
       break;
     default:
-      throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`); // play를 playFor() 호출로 변경
+      throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);  // play를 playFor() 호출로 변경
   }
 
   return result; // 함수 안에서 값이 바뀌는 변수 반환
