23dc9425e5d0c68f79dedac0dfe7563d44eedf30
HyoJin <121423610+oneday0230@users.noreply.github.com>
2023-02-12 21:40:49 +0900

    get amount()를 서브클래스에서 처리하도록 리팩토

--- chapter1/01-5/createStatementData.js
+++ chapter1/01-5/createStatementData.js
@@ -4,32 +4,6 @@ class PerformanceCaculator {  // p.66~ 공연료 계산기 생성
     this.play = aPlay;
   }
 
-  //amountFor함수를 복제하여 해당 클레스에서 계산 되도록 수정
-  get amount() {
-    let result = 0;
-    switch (this.play.type)
-    {
-      case 'tragedy': // 비극
-        result = 40_000;
-        if (this.performance.audience > 30) {
-          result += 1_000 * (this.performance.audience - 30);
-        }
-        break;
-      case 'comedy':  // 희극
-        result = 30_000;
-  
-        if (this.performance.audience > 20) {
-          result += 10_000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${this.play.type}`);
-    }
-  
-    return result;
-  }
-
   get volumeCredits() {
     let volumeCredits = 0;
     volumeCredits += Math.max(this.performance.audience - 30, 0);
@@ -43,7 +17,33 @@ class PerformanceCaculator {  // p.66~ 공연료 계산기 생성
 }
 
 function createPerformanceCaculator(aPerformance, aPlay) {
-  return new PerformanceCaculator(aPerformance, aPlay);
+  switch(aPlay.type) {
+    case 'tragedy': return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy': return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
+}
+
+class TragedyCalculator extends PerformanceCaculator {
+    get amount() {
+      let result = 40000;
+      if (this.performance.audience > 30) {
+        result += 1_000 * (this.performance.audience - 30);
+      }
+      return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCaculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
 }
 
 export default function createStatementData(invoice, plays) {
