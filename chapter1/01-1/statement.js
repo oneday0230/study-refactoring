3ec5341a4022317f8b9e3a9ea9739e72878f6154
HyoJin <121423610+oneday0230@users.noreply.github.com>
2023-02-12 22:06:33 +0900

    fix

--- chapter1/01-1/statement.js
+++ chapter1/01-1/statement.js
@@ -1,4 +1,5 @@
-export function statement(invoice, plays) {
+// base code
+function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
