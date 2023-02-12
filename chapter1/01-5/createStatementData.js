f3071cc1fac6c7de80222a0a7fad9b81de790d2f
HyoJin <121423610+oneday0230@users.noreply.github.com>
2023-02-12 22:01:56 +0900

    1.9 마무리 - 다형성을 활용하여 데이터 생성하기
    +코드정리

--- chapter1/01-5/createStatementData.js
+++ chapter1/01-5/createStatementData.js
@@ -1,11 +1,31 @@
-class PerformanceCaculator {  // p.66~ 공연료 계산기 생성
-  constructor(aPerformance, aPlay) {
-    this.performance = aPerformance;
-    this.play = aPlay;
+export default function createStatementData(invoice, plays) {
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+  return result;
+  
+  function enrichPerformance (aPerformance) {
+    const calculator = createPerformanceCaculator(aPerformance, playFor(aPerformance)); // 생성자 대신 팩터리 함수 이용
+    const result = Object.assign({}, aPerformance); // 얕은 복사 수행
+    result.play = calculator.play;
+    result.amount = calculator.amount;  // amountFor() 대신 클래스를 사용용하도록 수정
+    result.volumeCredits = calculator.volumeCredits;  //  volumeCreditsFor() 대신 클래스를 사용하도록 수정
+    return result;
   }
 
-  get volumeCredits() {
-    return Math.max(this.performance.audience - 30, 0);
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+  
+  function totalAmount(data) {
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+    return data.performances.reduce((total, p) => total + p.amount, 0);
+  }
+  
+  function totalVolumeCredits(data) {
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
 
@@ -15,6 +35,17 @@ function createPerformanceCaculator(aPerformance, aPlay) {
     case 'comedy': return new ComedyCalculator(aPerformance, aPlay);
     default:
       throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
+}
+
+class PerformanceCaculator {  // p.66~ 공연료 계산기 생성
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -40,45 +71,5 @@ class ComedyCalculator extends PerformanceCaculator {
 
   get volumeCredits() {
     return super.volumeCredits + Math.floor(this.performance.audience / 5);
-  }
-}
-
-export default function createStatementData(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
-  
-  function enrichPerformance (aPerformance) {
-    const calculator = createPerformanceCaculator(aPerformance, playFor(aPerformance)); // 생성자 대신 팩터리 함수 이용
-    const result = Object.assign({}, aPerformance); // 얕은 복사 수행
-    result.play = calculator.play;
-    result.amount = calculator.amount;  // amountFor() 대신 클래스를 사용용하도록 수정
-    result.volumeCredits = calculator.volumeCredits;  //  volumeCreditsFor() 대신 클래스를 사용하도록 수정
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-  
-  function amountFor(aPerformance) {
-    // 원본함수인 amountFor()도 계산기를 이용하도록 수정
-    return new PerformanceCaculator(aPerformance, playFor(aPerformance)).amount;
-  }
-  
-  function volumeCreditsFor(aPerformance) {
-    return new PerformanceCaculator(aPerformance, playFor(aPerformance)).volumeCredits;
-  }
-  
-  function totalAmount(data) {
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-  }
-  
-  function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
